7522929333ea44977631ad93a137606d2c669576
EderSampayo <edersampayoupv@gmail.com>
2024-04-22 19:03:50 +0200

    Quitar comentarios

--- app/src/main/java/com/example/das_animales/MiFirebaseMessagingService.kt
+++ app/src/main/java/com/example/das_animales/MiFirebaseMessagingService.kt
@@ -15,13 +15,6 @@ class MiFirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onNewToken(token: String) {
         Log.d(TAG, "Refreshed token: $token")
-
-        // If you want to send messages to this application instance or
-        // manage this apps subscriptions on the server side, send the
-        // FCM registration token to your app server.
-
-        //TODO:
-        //sendRegistrationToServer(token)
     }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
